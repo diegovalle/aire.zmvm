6ea215faac88da86e487a01a51fe104f2b82163b
Diego Valle-Jones <diegovalle@gmail.com>
2016-03-18 20:02:07 -0600

    stations

---
+++ R/stations.R
@@ -0,0 +1,16 @@
+#' Pollution measuring stations in Mexico City
+#'
+#'
+#' @format A data frame with 63 rows and 7 variables:
+#' \describe{
+#'   \item{cve_estac}{abbreviation of the station}
+#'   \item{nom_estac}{name of the station}
+#'   \item{longitud}{longitud of the station}
+#'   \item{latitud}{latitude of the station}
+#'   \item{alt}{altitude of the station}
+#'   \item{obs_estac}{comment}
+#'   \item{id_station}{id of the station}
+#'
+#' }
+#' @source \url{http://www.aire.df.gob.mx/default.php?opc=%27aKBhnmI=%27&opcion=Zg==}
+"stations"
