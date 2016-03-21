7e2a364c55de93080792efe1ebea6548e25c4175
Diego Valle-Jones <diegovalle@gmail.com>
2016-03-20 19:42:41 -0600

    download zone data

---
+++ R/get_station_data.R
@@ -0,0 +1,106 @@
+
+
+#' Title
+#'
+#' @param criterion bla
+#' @param pollutant bla
+#' @param year bla
+#'
+#' @importFrom stringr str_c  str_replace_all
+#' @importFrom rvest html_nodes html_table
+#' @importFrom xml2 read_html
+#' @importFrom dplyr %>%
+#'
+clean_aire <- function(criterion, pollutant, year) {
+  base_url = "http://www.aire.df.gob.mx/estadisticas-consultas/concentraciones/respuesta.php?"
+  url <- str_c(base_url, "qtipo=", criterion, "&",
+               "parametro=", pollutant, "&",
+               "anio=", year, "&",
+               "qmes=")
+  poll_table <- read_html(url)
+  df <- html_table(html_nodes(poll_table, "table")[[1]], header = TRUE)
+  names(df) <- df[1,]
+  names(df)[1] <- "date"
+  names(df) <- str_replace_all(names(df), "\\s", "")
+  df <- df[2:nrow(df),]
+
+  df[df == "nr"] <- NA
+  df[,2:ncol(df)] <- apply(df[,2:ncol(df)], 2, as.numeric)
+  # when the data is HORARIOS the second column corresponds to the hour
+  if(criterion != "HORARIOS") {
+    idx = 2
+  } else {
+    idx = 3
+    names(df)[2] <- "hour"
+  }
+  # df$max <- apply(df[ ,idx:ncol(df)], 1, max, na.rm = TRUE)
+  # df$min <- apply(df[ ,idx:ncol(df)], 1, min, na.rm = TRUE)
+  # df$mean <- apply(df[ ,idx:ncol(df)], 1, mean, na.rm = TRUE)
+  # df$median <- apply(df[ ,idx:ncol(df)], 1, median, na.rm = TRUE)
+  return(df)
+}
+
+#' Download pollution data
+#'
+#' retrieve pollution data from the air quality server at \url{
+#' http://www.aire.df.gob.mx/estadisticas-consultas/concentraciones/index.php}
+#'
+#' @param criterion Type of data to download.
+#' \itemize{
+#'  \item{"HORARIOS"}{ - Hourly data}
+#'  \item{"MAXIMOS""}{ - Daily maximums}
+#'  \item{"MINIMOS"}{ - Daily minimums}
+#' }
+#' @param pollutant The type of pollutant to download.
+#' \itemize{
+#'  \item{"SO2"}{ - Dioxido de azufre}
+#'  \item{"CO"}{ - Monoxido de carbono}
+#'  \item{"NOX"}{ - Oxidos de nitrogeno}
+#'  \item{"NO2"}{ - Dioxido de nitrogeno}
+#'  \item{"NO"}{ - Oxido nitrico}
+#'  \item{"O3"}{ - Ozono}
+#'  \item{"PM10"}{ - Particulas menores a 10 micrometros}
+#'  \item{"PM2"}{ - Particulas menores a 2.5 micrometros}
+#'  \item{"WSP"}{ - Velocidad del viento}
+#'  \item{"WDR"}{ - Direccion del viento}
+#'  \item{"TMP"}{ - Temperatura ambiente}
+#'  \item{"RH"}{ - Humedad relativa}
+#' }
+#' @param year a numeric vector containing the years for which to download data
+#'
+#' @return a data.frame with pollution data
+#'
+#' @export
+#' @importFrom data.table rbindlist
+#' @importFrom lubridate dmy
+#' @importFrom tidyr gather_
+#'
+#' @examples
+#' \dontrun{
+#' # Download daily maximum PM10 data (particulate matter 10 micrometers or less in diameter)
+#' # from 2015 to 2016
+#' df <- get_station_data("MAXIMOS", "pm10", 2015:2016)
+#' head(df)
+#' }
+get_station_data <- function(criterion, pollutant, year) {
+  stopifnot(criterion %in% c("HORARIOS", "MAXIMOS", "MINIMOS"))
+  pollutant <- tolower(pollutant)
+  stopifnot(pollutant %in% c("so2", "co", "nox", "no2",
+                             "no", "o3", "pm10", "pm2",
+                             "wsp", "wdr", "tmp", "rh"))
+  stopifnot(min(year)>=2005)
+  ll <- mapply(clean_aire,
+               criterio = rep(criterion, length(year)),
+               pollutant = rep(pollutant, length(year)),
+               year = year,
+               SIMPLIFY = FALSE)
+  df <- rbindlist(ll)
+  df$date <- as.Date(dmy(df$date))
+  if(criterion != "HORARIOS") {
+    val_cols <- base::setdiff(names(df), c("date"))
+  } else {
+    val_cols <- base::setdiff(names(df), c("date", "hour"))
+  }
+  gather_(df, "station", "value", val_cols)
+}
+
---
+++ R/get_zone_data.R
@@ -0,0 +1,135 @@
+
+#' Title
+#'
+#' @param pollutant bla
+#' @param zone bla
+#' @param start_date bla
+#' @param end_date bla
+#' @param criterion bla
+#'
+#' @return data.frame
+#'
+#' @importFrom stringr str_c  str_replace_all
+#' @importFrom rvest html_nodes html_table
+#' @importFrom lubridate day month year
+#' @importFrom httr content
+download_zone <- function(criterion, pollutant, zone, start_date, end_date) {
+  url = "http://www.aire.df.gob.mx/estadisticas-consultas/consultas/resultado_consulta.php"
+  fd <- list(
+    diai	= day(start_date),
+    mesi	= month(start_date),
+    anoi	= year(start_date),
+    diaf	= day(end_date),
+    mesf	= month(end_date),
+    anof	= year(end_date),
+    #pollutant = "on",
+    #zone = "on",
+    Q	= criterion,
+    inter	= "",
+    consulta = "Consulta"
+  )
+  pollutant_tmp <- rep("on", length(pollutant))
+  names(pollutant_tmp) <- pollutant
+  fd <- append(fd, pollutant_tmp)
+  zones_tmp <- rep("on", length(zone))
+  names(zones_tmp) <- zone
+  fd <- append(fd, zones_tmp)
+
+  result <- httr::POST(url,
+                       body = fd,
+                       encode = "form")
+  poll_table <- xml2::read_html(content(result, "text"))
+
+  df <- rvest::html_table(rvest::html_nodes(poll_table, "table")[[1]], header = TRUE)
+  df
+}
+
+#' Download pollution data by zone
+#'
+#' @param pollutant The type of pollutant to download
+#' \itemize{
+#'  \item{"SO2"}{ - Dioxido de azufre}
+#'  \item{"CO"}{ - Monoxido de carbono}
+#'  \item{"NO2"}{ - Dioxido de nitrogeno}
+#'  \item{"O3"}{ - Ozono}
+#'  \item{"PM10"}{ - Particulas menores a 10 micrometros}
+#'  \item{"TC"}{All the pollutants}
+#' }
+#' @param zone The geographic zone for which to download data
+#' \itemize{
+#'  \item{"NO"}{ - Noroeste}
+#'  \item{"NE"}{ - Noreste}
+#'  \item{"CE"}{ - Centro}
+#'  \item{"SO"}{ - Suroeste}
+#'  \item{"SE"}{ - Sureste}
+#'  \item{"TZ"}{ - All the zones}
+#' }
+#' @param criterion The type of data to download
+#' \itemize{
+#'  \item{"HORARIOS"}{ - Hourly data}
+#'  \item{"MAXIMOS""}{ - Daily maximums}
+#' }
+#' @param start_date The start date in YYYY-MM-DD format.
+#' @param end_date The end date in YYYY-MM-DD format.
+#'
+#' @return A data.frame with pollution data measured in IMECAS, by geographic zone
+#'
+#' @export
+#' @importFrom stringr str_c  str_replace_all
+#' @importFrom rvest html_nodes html_table
+#' @importFrom xml2 read_html
+#' @importFrom tidyr gather_ separate_
+#'
+#' @examples
+#' \dontrun{
+#' # Download daily maximum PM10 data (particulate matter 10 micrometers or less in diameter)
+#' # from 2015-01-01 to 2016-03-20
+#' df <- get_zone_data("MAXIMOS", "PM10", "TZ", "2015-01-01", "2016-03-20")
+#' head(df)
+#' }
+#'
+get_zone_data <- function(criterion, pollutant, zone, start_date, end_date) {
+  if (missing(pollutant))
+    stop("You need to specify a contaminante")
+  if (missing(zone))
+    stop("You need to specify a zona")
+  if (missing(criterion))
+    stop("You need to specify a start date")
+  if (missing(end_date))
+    stop("You need to specify an end date (YYYY-MM-DD)")
+  if (missing(start_date))
+    stop("You need to specify a start date (YYYY-MM-DD)")
+  criterion <- toupper(criterion)
+  stopifnot(length(base::setdiff(pollutant,
+                                 c("SO2", "CO", "NO2", "O3", "PM10", "TC"))) == 0)
+  stopifnot(length(base::setdiff(zone,
+                                 c("NO", "NE", "CE", "SO", "SE", "TZ"))) == 0)
+  stopifnot(criterion %in% c("HORARIOS", "MAXIMOS"))
+  criterion <- tolower(criterion)
+
+  df <- download_zone(criterion, pollutant, zone, start_date, end_date)
+
+  names(df) <- df[1,]
+  names(df)[1] <- "date"
+  names(df) <- str_replace_all(names(df), "\\s", "")
+  df <- df[2:nrow(df),]
+
+  # when the data is HORARIOS the second column corresponds to the hour
+  if(criterion != tolower("HORARIOS")) {
+    value_col = base::setdiff(names(df), "date")
+  } else {
+    names(df)[2] <- "hour"
+    value_col = base::setdiff(names(df), c("date", "hour"))
+  }
+  df <- df %>%
+    gather_("zone_pollutant", "value", value_col) %>%
+    separate_("zone_pollutant", c("zone", "pollutant"), sep = 2)
+  # Some  values are invalid and to avoid warnings I correct them manually
+  df[which(df$value == ""), "value"] <- NA
+  df[which(df$value == "M"), "value"] <- NA
+  df$value <- as.numeric(df$value)
+  df$date <- as.Date(df$date)
+  df
+}
+
+
