62b2f275deb6627ce9fb32a6037cca3d7e5df66d
Diego Valle-Jones <diegovalle@gmail.com>
2016-03-22 23:04:49 -0600

    column names changed

--- R/stations.R
+++ R/stations.R
@@ -3,13 +3,13 @@
 #'
 #' @format A data frame with 63 rows and 7 variables:
 #' \describe{
-#'   \item{cve_estac}{abbreviation of the station}
-#'   \item{nom_estac}{name of the station}
-#'   \item{longitud}{longitud of the station}
-#'   \item{latitud}{latitude of the station}
-#'   \item{alt}{altitude of the station}
-#'   \item{obs_estac}{comment}
-#'   \item{id_station}{id of the station}
+#'   \item{station_code}{abbreviation of the station}
+#'   \item{station_name}{name of the station}
+#'   \item{lon}{longitud of the station}
+#'   \item{lat}{latitude of the station}
+#'   \item{altitude}{altitude of the station}
+#'   \item{comment}{comment}
+#'   \item{station_id}{id of the station}
 #'
 #' }
 #' @source \url{http://148.243.232.112:8080/opendata/catalogos/cat_estacion.csv}
