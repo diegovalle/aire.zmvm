9fc2610f23f5f5244949f186df0b0d099f9de643
Diego Valle-Jones <diegovalle@gmail.com>
2016-05-23 19:30:01 -0500

    timeout and correctly return time when the timestamp equals 24h

--- R/get_latest_data.R
+++ R/get_latest_data.R
@@ -6,13 +6,14 @@
 
 convert_time <- function(time){
   time <- str_replace_all(time, "\n|\t", "")
-  time <- str_replace(time, ",[ A-Za-z\u00e1\u00e9\u00ed\u00f3\u00fa\u00c1\u00c9\u00cd\u00d3\u00da]+ ", "")
+  time <- str_replace(time, ",[ &;A-Za-z\u00e1\u00e9\u00ed\u00f3\u00fa\u00c1\u00c9\u00cd\u00d3\u00da]+ ", "")
   month_names <- c('enero'='january','febrero'='february','marzo'='march',
                    'abril'='april','mayo'='may','junio'='june','julio'='july',
                    'agosto'='august','septiembre'='september',
                    'octubre'='october','noviembre'='november','diciembre'='december')
   time <- str_replace_all(time, month_names)
-  as.character(strptime(time, "%H:%M h%d de %B de %Y ", tz = "America/Mexico_City"))
+  time <- strptime(time, "%H:%M h%d de %B de %Y ", tz = "America/Mexico_City")
+  as.character(strftime(time, '%Y-%m-%d %H:%M:%S'))
 }
 
 #' Get the latest pollution values for each station
@@ -24,6 +25,7 @@ convert_time <- function(time){
 #' @importFrom rvest html_nodes html_text
 #' @importFrom xml2 read_html
 #' @importFrom stringr str_match str_replace str_replace_all
+#' @importFrom httr GET
 #'
 #' @examples
 #' \dontrun{
@@ -33,7 +35,7 @@ convert_time <- function(time){
 get_latest_data <- function() {
   url = "http://www.aire.df.gob.mx/ultima-hora-reporte.php"
 
-  poll_table <- read_html(url)
+  poll_table <- read_html(httr::GET(url,  httr::timeout(60)))
   time <- convert_time(html_text(html_nodes(poll_table, "div#textohora")))
 
   df <- html_table(html_nodes(poll_table, "table")[[1]], header = TRUE, fill = TRUE)
