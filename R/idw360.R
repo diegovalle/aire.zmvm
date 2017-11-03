81362ca534d0dc512ab8adf5ff98c6641e9c9b19
diego <diegovalle@gmail.com>
2017-11-02 21:14:16 -0600

    Inverse Distance Weighting with Directional Data

---
+++ R/idw360.R
@@ -0,0 +1,45 @@
+#' Inverse Distance Weighting with Directional Data
+#'
+#' function for inverse distance weighted interpolation with directional data. Useful for when you
+#' are working with data whose unit of measurement is degrees (i.e. the average of 35° and 355°
+#' should be 15°). It works by finding the shortest distance between two degree marks
+#' on a circle
+#'
+#' @param values the dependent variable
+#' @param coords the spatial data locations (coordinates)
+#' @param grid data frame or Spatial object with prediction/simulation locations
+#' @param idp The inverse distance weighting power
+#'
+#' @importFrom sp spDists
+#'
+#' @return data.frame with the interpolated values for each of the grid points
+#' @export
+#'
+idw360 <- function(values, coords, grid, idp = 2) {
+  stopifnot(length(values) == nrow(coords))
+  stopifnot(is.numeric(idp))
+  distance <- t(spDists(coords, grid))
+  w <- 1 / (distance ^ idp)
+
+  for (i in 1:nrow(w)) {
+    if (sum(is.infinite(w[i, ])) > 0){
+      w[i, !is.infinite(w[i, ])] <- 0
+      w[i, is.infinite(w[i, ])] <- 1
+    }
+  }
+
+  y <- (sin(values * (pi / 180)))
+  w.sum <- apply(w, 1, sum, na.rm = TRUE)
+  wy <- w %*% diag(y)
+  uy <- apply(wy / w.sum, 1, sum, na.rm = TRUE)
+
+  x <- (cos(values * (pi / 180)))
+  w.sum <- apply(w, 1, sum, na.rm = TRUE)
+  wx <- w %*% diag(x)
+  ux <- apply(wx / w.sum, 1, sum, na.rm = TRUE)
+
+  res <- atan2(uy, ux) * (180 / pi)
+  res <- ifelse(res < 0, 360 + res, res)
+
+  data.frame(pred = res)
+}
