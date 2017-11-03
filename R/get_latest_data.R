4c80603078469aa93d93278a058b56c5d6abb922
diego <diegovalle@gmail.com>
2017-11-03 14:27:46 -0600

    print encoding

--- R/get_latest_data.R
+++ R/get_latest_data.R
@@ -5,7 +5,10 @@
 #' @importFrom stringr str_match str_replace str_replace_all str_detect
 
 .convert_time <- function(time){
+  print(Encoding(time))
+  time <- iconv(time, "UTF-8", "ASCII\\TRANSLIT")
   print(time)
+
   time <- str_replace_all(time, "\n|\t", "")
   print(time)
   time <- str_replace(time, ",[ &;A-Za-z\u00e1\u00e9\u00ed\u00f3\u00fa\u00c1\u00c9\u00cd\u00d3\u00da]+ ",
