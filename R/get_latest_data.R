df088900784aae2ce62ebd5f37897e7450058e4a
diego <diegovalle@gmail.com>
2017-11-06 17:19:18 -0600

    stri_translate_general instead of iconv to remove accents

--- R/get_latest_data.R
+++ R/get_latest_data.R
@@ -3,9 +3,10 @@
 #' @param time time to convert
 #'
 #' @importFrom stringr str_match str_replace str_replace_all str_detect
-
+#' @importFrom stringi stri_trans_general
 .convert_time <- function(time){
-  time <- iconv(time, "UTF-8", "ASCII//TRANSLIT")
+  time <- stri_trans_general(time, "Latin-ASCII")
+  #time <- iconv(time, "UTF-8", "ASCII//TRANSLIT")
   time <- str_replace_all(time, "\n|\t", "")
   time <- str_replace(time, ",[ &;'A-Za-z\u00e1\u00e9\u00ed\u00f3\u00fa\u00c1\u00c9\u00cd\u00d3\u00da]+ ",
                       "")
