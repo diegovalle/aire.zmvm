161f0fb5fb4132abb515c0c70774de2588c7b61e
diego <diegovalle@gmail.com>
2018-03-04 20:41:26 -0600

    define the pollution zones by municipio

---
+++ R/zones.R
@@ -0,0 +1,30 @@
+#' Pollution zones in Mexico City
+#'
+#' This data set contains the municipios (counties) that make up the 5
+#' geographic zones into which Mexico City was
+#' divided for the purpose of informing about the IMECA index.
+#'
+#' Note that in
+#' 2015 it was determined that the stations with codes ACO, AJU, INN, MON
+#' and MPA would no longer be taken into consideration when computing the
+#' pollution index because they didn't meet the
+#' \href{http://www.aire.cdmx.gob.mx/objetivos-monitoreo-calidad-aire.html}{objectives of monitoring
+#' air quality} and are no longer included in the index even if they are still
+#' part of the SIMAT (Sistema de Monitoreo Atmosférico de la Ciudad de México)
+#'
+#' @format A data frame with 36 rows and 6 variables:
+#' \describe{
+#'   \item{region}{INEGI code of the region (state_code + municipio_code)}
+#'   \item{state_code}{INEGI code of the state}
+#'   \item{state_abbr}{state abbreviation}
+#'   \item{municipio_code}{INEGI code of the municipio}
+#'   \item{municipio_name}{name of the municipio}
+#'   \item{zone}{zone}
+#'
+#' }
+#' @source \href{http://www.aire.cdmx.gob.mx/descargas/ultima-hora/calidad-aire/pcaa/Gaceta_Oficial_CDMX.pdf}{ Gaceta Oficial de la Ciudad de México}
+#' No. 230, 27 de Diciembre de 2016 and
+#' \emph{Solicitud de Información} FOLIO 0112000033818
+#' @examples
+#' head(zones)
+"zones"
